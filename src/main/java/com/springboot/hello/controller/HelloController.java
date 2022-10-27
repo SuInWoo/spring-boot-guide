1ffdeabd40169b04b6a04153f02cde2470048898
SuInWoo <wbo1026@naver.com>
2022-10-27 11:49:00 +0900

    @RequestParam Test2 - use Map<String, String> - return StringBuilder

--- src/main/java/com/springboot/hello/controller/HelloController.java
+++ src/main/java/com/springboot/hello/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.springboot.hello.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/v1/get-api")
 public class HelloController {
@@ -30,6 +32,14 @@ public class HelloController {
         return String.format("%s, %s, %s", name, email, organization);
     }
 
+    @GetMapping(value = "/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param){
+        StringBuilder sb = new StringBuilder();
 
+        param.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
 
+        return sb.toString();
+    }
 }
