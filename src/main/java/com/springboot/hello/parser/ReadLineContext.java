5cf180ccaf5d415c18098876f15e09841b1ba556
SuInWoo <wbo1026@naver.com>
2022-10-31 15:47:04 +0900

    첫 번째 line을 저장하지 않도록 수정

--- src/main/java/com/springboot/hello/parser/ReadLineContext.java
+++ src/main/java/com/springboot/hello/parser/ReadLineContext.java
@@ -18,6 +18,7 @@ public class ReadLineContext<T> {
         List<T> result = new ArrayList<>(); //결과를 담을 ArrayList
         BufferedReader reader = new BufferedReader(new FileReader(filename));   //File 읽어옴
         String str;
+        reader.readLine();  //첫 줄은 저장하지 않도록 처리
         while ((str = reader.readLine()) != null) { //한 줄씩 읽어 result에 저장
             try {
                 result.add(parser.parse(str));
