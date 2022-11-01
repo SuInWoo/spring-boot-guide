a1a69a4807ed04b5772c85c5cf22f2b04b114ca0
SuInWoo <wbo1026@naver.com>
2022-11-01 15:28:43 +0900

    HospitalDao findById 추가 - Test 추가 - LocalDateTime의 경우 getObject 활용 참고 사이트: https://stackoverflow.com/questions/15784069/getdatetime-from-resultset-java

--- src/test/java/com/springboot/hello/parser/HospitalParserTest.java
+++ src/test/java/com/springboot/hello/parser/HospitalParserTest.java
@@ -33,49 +33,11 @@ class HospitalParserTest {
     HospitalDao hospitalDao;
 
     @Test
-    @DisplayName("10만건 이상 데이터가 파싱 되는지")
-    void oneHundredParserTest() throws IOException {
-        String filename = "/Users/suin/Downloads/수업 데이터 파일/fulldata_01_01_02_P_의원.csv";
-        List<Hospital> hospitalList = hospitalReadLineContext.readByLine(filename);
-        assertTrue(hospitalList.size() > 1000);
-        assertTrue(hospitalList.size() > 100000);
-    }
-
-    @Test
-    @DisplayName("csv 1줄 -> Hospital")
-    void convertToHospital() {
-
-        HospitalParser hp = new HospitalParser();
-        Hospital hospital = hp.parse(line1);
-
-        assertEquals(1, hospital.getId());
-        assertEquals("의원", hospital.getOpenServiceName());
-        assertEquals(3620000,hospital.getOpenLocalGovernmentCode());
-        assertEquals("PHMA119993620020041100004",hospital.getManagementNumber());
-        assertEquals(LocalDateTime.of(1999, 6, 12, 0, 0),
-                hospital.getLicenseDate());
-        assertEquals(1, hospital.getBusinessStatus());
-        assertEquals(13, hospital.getBusinessStatusCode());
-        assertEquals("062-515-2875", hospital.getPhone());
-        assertEquals("광주광역시 북구 풍향동 565번지 4호 3층", hospital.getFullAddress());
-        assertEquals("광주광역시 북구 동문대로 24, 3층 (풍향동)", hospital.getRoadNameAddress());
-        assertEquals("효치과의원", hospital.getHospitalName());
-        assertEquals("치과의원", hospital.getBusinessTypeName());
-        assertEquals(1, hospital.getHealthcareProviderCount());
-        assertEquals(0, hospital.getPatientRoomCount());
-        assertEquals(0, hospital.getTotalNumberOfBeds());
-        assertEquals(52.29f, hospital.getTotalAreaSize());
-
-    }
-
-    @Test
     @DisplayName("Hospital이 insert 잘 되는지")
     void add(){
         HospitalParser hp = new HospitalParser();
         Hospital hospital = hp.parse(line1);
         hospitalDao.add(hospital);
-
-        //findById, deleteAll, getCount 구현 필요
     }
 
     @Test
@@ -110,8 +72,48 @@ class HospitalParserTest {
         hospitalDao.add(hospital);
 
         Hospital hospitalTest = hospitalDao.findById(1);
-        assertThat(hospitalTest.getId()).isEqualTo(1);
-        assertThat(hospitalTest.getLicenseDate())
+        assertThat(hospitalTest.getId()).isEqualTo(1);  //int Test
+        assertThat(hospitalTest.getLicenseDate())   //localDateTime Test
                 .isEqualTo(hospital.getLicenseDate());
+        assertThat(hospitalTest.getTotalAreaSize()) //float Test
+                .isEqualTo(hospital.getTotalAreaSize());
+    }
+
+
+
+    @Test
+    @DisplayName("10만건 이상 데이터가 파싱 되는지")
+    void oneHundredParserTest() throws IOException {
+        String filename = "/Users/suin/Downloads/수업 데이터 파일/fulldata_01_01_02_P_의원.csv";
+        List<Hospital> hospitalList = hospitalReadLineContext.readByLine(filename);
+        assertTrue(hospitalList.size() > 1000);
+        assertTrue(hospitalList.size() > 100000);
+    }
+
+    @Test
+    @DisplayName("csv 1줄 -> Hospital")
+    void convertToHospital() {
+
+        HospitalParser hp = new HospitalParser();
+        Hospital hospital = hp.parse(line1);
+
+        assertEquals(1, hospital.getId());
+        assertEquals("의원", hospital.getOpenServiceName());
+        assertEquals(3620000,hospital.getOpenLocalGovernmentCode());
+        assertEquals("PHMA119993620020041100004",hospital.getManagementNumber());
+        assertEquals(LocalDateTime.of(1999, 6, 12, 0, 0),
+                hospital.getLicenseDate());
+        assertEquals(1, hospital.getBusinessStatus());
+        assertEquals(13, hospital.getBusinessStatusCode());
+        assertEquals("062-515-2875", hospital.getPhone());
+        assertEquals("광주광역시 북구 풍향동 565번지 4호 3층", hospital.getFullAddress());
+        assertEquals("광주광역시 북구 동문대로 24, 3층 (풍향동)", hospital.getRoadNameAddress());
+        assertEquals("효치과의원", hospital.getHospitalName());
+        assertEquals("치과의원", hospital.getBusinessTypeName());
+        assertEquals(1, hospital.getHealthcareProviderCount());
+        assertEquals(0, hospital.getPatientRoomCount());
+        assertEquals(0, hospital.getTotalNumberOfBeds());
+        assertEquals(52.29f, hospital.getTotalAreaSize());
+
     }
 }
