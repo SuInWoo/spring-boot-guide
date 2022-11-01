7624a700cda2c9e2aa2683b6b66d748eaf316b7b
SuInWoo <wbo1026@naver.com>
2022-11-01 15:45:59 +0900

    HospitalDao findById 추가 - Test 추가 - LocalDateTime의 경우 getObject 활용 참고 사이트: https://stackoverflow.com/questions/15784069/getdatetime-from-resultset-java

--- src/test/java/com/springboot/hello/parser/HospitalParserTest.java
+++ src/test/java/com/springboot/hello/parser/HospitalParserTest.java
@@ -2,7 +2,6 @@ package com.springboot.hello.parser;
 
 import com.springboot.hello.dao.HospitalDao;
 import com.springboot.hello.domain.Hospital;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
